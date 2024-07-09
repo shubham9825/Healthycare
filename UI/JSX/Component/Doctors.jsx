f8698f2a15b8ce3ed47dbfd3144ea859ab9fbfa4
Shubham <shubhamsheliya9825@gmail.com>
2024-07-09 15:00:23 -0400

    HLTHCR-23 Homepage design

---
+++ UI/JSX/Component/Doctors.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { DoctorCard } from "../Common/DoctorCard.jsx";
+import profile1 from "../../Assets/Images/profile-1.png";
+import profile2 from "../../Assets/Images/profile-2.png";
+import profile3 from "../../Assets/Images/profile-3.png";
+import profile4 from "../../Assets/Images/profile-4.png";
+import "../../Assets/Styles/Doctors.css";
+
+const doctorDetails = [
+  {
+    img: profile1,
+    name: "Dr. Kathryn Murphy",
+    title: "General Surgeons",
+    stars: "4.9",
+    reviews: "1800",
+  },
+  {
+    img: profile2,
+    name: "Dr. Jacob Jones",
+    title: "Hematologists",
+    stars: "4.8",
+    reviews: "700",
+  },
+  {
+    img: profile3,
+    name: "Dr. Jenny Wilson",
+    title: "Endocrinologists",
+    stars: "4.7",
+    reviews: "450",
+  },
+  {
+    img: profile4,
+    name: "Dr. Albert Flores",
+    title: "Hematologists",
+    stars: "4.8",
+    reviews: "500",
+  },
+];
+
+export const Doctors = () => {
+  return (
+    <div className="doctor-section" id="doctors">
+      <div className="dt-title-content">
+        <h3 className="dt-title">
+          <span>Meet Our Doctors</span>
+        </h3>
+
+        <p className="dt-description">
+          Meet our exceptional team of specialist doctors, dedicated to
+          providing top-notch healthcare services at HealthCare Life. Trust in
+          their knowledge and experience to lead you towards a healthier and
+          happier life.
+        </p>
+      </div>
+
+      <div className="dt-cards-content">
+        {doctorDetails.map((data, i) => (
+          <DoctorCard
+            key={i}
+            img={data.img}
+            name={data.name}
+            title={data.title}
+            stars={data.stars}
+            reviews={data.reviews}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
--- UI/JSX/Routing.jsx
+++ UI/JSX/Routing.jsx
@@ -1,110 +1,175 @@
-import React, { Suspense, lazy } from 'react';
-import { Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
-import NotFound from './Component/NotFound.jsx';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { showToast } from './utils/toastService.js';
+import React, { Suspense, lazy, useState } from "react";
+import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
+import NotFound from "./Component/NotFound.jsx";
+import { showToast } from "./utils/toastService.js";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
+import "../Assets/Styles/Navbar.css";
+import logo from "../Assets/Images/healthyCareLife.png";
 
-const Dashboard = lazy(() => retry(() => import("./Component/Dashboard.jsx")))
-const SignUp = lazy(() => retry(() => import("./Component/SignUp.jsx")))
-const SignIn = lazy(() => retry(() => import("./Component/SignIn.jsx")))
-const Appointment = lazy(() => retry(() => import("./Component/Appointment.jsx")))
+const Dashboard = lazy(() => retry(() => import("./Component/Dashboard.jsx")));
+const SignUp = lazy(() => retry(() => import("./Component/SignUp.jsx")));
+const SignIn = lazy(() => retry(() => import("./Component/SignIn.jsx")));
+const Appointment = lazy(() =>
+  retry(() => import("./Component/Appointment.jsx"))
+);
 
 const retry = (lazyComponent, attemptsLeft = 2) => {
-    return new Promise((resolve, reject) => {
-        lazyComponent()
-            .then(resolve)
-            .catch((error) => {
-                setTimeout(() => {
-                    if (attemptsLeft === 1) {
-                        reject(error);
-                        return;
-                    }
-                    retry(lazyComponent, attemptsLeft - 1).then(resolve, reject);
-                }, 1000);
-            });
-    });
-}
+  return new Promise((resolve, reject) => {
+    lazyComponent()
+      .then(resolve)
+      .catch((error) => {
+        setTimeout(() => {
+          if (attemptsLeft === 1) {
+            reject(error);
+            return;
+          }
+          retry(lazyComponent, attemptsLeft - 1).then(resolve, reject);
+        }, 1000);
+      });
+  });
+};
 
 const routes = [
-    {
-        path: "/",
-        component: Dashboard,
-        isAuth: false,
-    },
-    {
-        path: "/SignUp",
-        component: SignUp,
-        isAuth: false,
-    },
-    {
-        path: "/SignIn",
-        component: SignIn,
-        isAuth: false,
-    },
-    {
-        path: "/Appointment",
-        component: Appointment,
-        isAuth: true,
-    },
-]
+  {
+    path: "/SignUp",
+    component: SignUp,
+    isAuth: false,
+  },
+  {
+    path: "/SignIn",
+    component: SignIn,
+    isAuth: false,
+  },
+  {
+    path: "/Appointment",
+    component: Appointment,
+    isAuth: true,
+  },
+];
 
 const isAuthenticated = () => {
-    return !!localStorage.getItem('token');
-}
+  return !!localStorage.getItem("token");
+};
 
 const ProtectedRoute = ({ element: Component, isAuth, ...rest }) => {
-    if (isAuth && !isAuthenticated()) {
-        return <Navigate to="/signin" />;
-    }
-    return <Component {...rest} />;
-}
+  if (isAuth && !isAuthenticated()) {
+    return <Navigate to="/signin" />;
+  }
+  return <Component {...rest} />;
+};
 
 export default function Routing() {
-    const navigate = useNavigate();
-    const filteredAuthRoutes = routes.filter(route => isAuthenticated() ? route.isAuth : !route.isAuth);
+  const [nav, setNav] = useState(false);
 
-    const handleLogout = () => {
-        localStorage.removeItem('token');
-        navigate('/signin');
-        showToast("Logout Successfully!", 'success')
-    };
+  const openNav = () => {
+    setNav(!nav);
+  };
 
-    return (
-        <>
-            <Navbar bg="primary" data-bs-theme="dark" className='navbar'>
-                <Container>
-                    <Navbar.Brand href="/">Hospital Management</Navbar.Brand>
-                    <Nav className="me-auto">
-                        {filteredAuthRoutes.map((route, index) => (
-                            <Nav.Link as="div" key={index}>
-                                <Link to={route.path}>
-                                    {route.path === "/" ? "Dashboard" : route.path.substring(1)}
-                                </Link> &nbsp;
-                            </Nav.Link>
-                        ))}
-                        {isAuthenticated() && (
-                            <Nav.Link as="div" onClick={handleLogout}>
-                                Logout
-                            </Nav.Link>
-                        )}
-                    </Nav>
-                </Container>
-            </Navbar>
+  const navigate = useNavigate();
+  const filteredAuthRoutes = routes.filter((route) =>
+    isAuthenticated() ? route.isAuth : !route.isAuth
+  );
 
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    {routes.map((route, index) => (
-                        <Route
-                            key={index}
-                            path={route.path}
-                            element={<ProtectedRoute element={route.component} isAuth={route.isAuth} />}
-                        />
-                    ))}
-                    <Route path="*" element={<NotFound />} />
-                </Routes>
-            </Suspense>
-        </>
-    );
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/signin");
+    showToast("Logout Successfully!", "success");
+  };
+
+  return (
+    <>
+      <div className="navbar-section">
+        <h1 className="navbar-title">
+          <Link to="/">
+            <img src={logo} alt="HealthyCareLife" className="ft-logo" />
+          </Link>
+        </h1>
+
+        {/* Desktop */}
+        <ul className="navbar-items">
+          <li>
+            <Link className="navbar-links" to={"/"}>
+              Dashboard
+            </Link>
+          </li>
+          {filteredAuthRoutes.map((route, index) => (
+            <li key={index}>
+              <Link to={route.path} className="navbar-links">
+                {route.path === "/" ? "Dashboard" : route.path.substring(1)}
+              </Link>
+            </li>
+          ))}
+          {isAuthenticated() && (
+            <li>
+              <Link className="navbar-links" onClick={handleLogout}>
+                Logout
+              </Link>
+            </li>
+          )}
+        </ul>
+
+        {/* Mobile */}
+        <div className={`mobile-navbar ${nav ? "open-nav" : ""}`}>
+          <div onClick={openNav} className="mobile-navbar-close">
+            <FontAwesomeIcon icon={faXmark} className="hamb-icon" />
+          </div>
+
+          <ul className="mobile-navbar-links">
+            <li>
+              <Link className="navbar-links" onClick={openNav} to={"/"}>
+                Dashboard
+              </Link>
+            </li>
+            {filteredAuthRoutes.map((route, index) => (
+              <li key={index}>
+                <Link
+                  to={route.path}
+                  onClick={openNav}
+                  className="navbar-links"
+                >
+                  {route.path === "/" ? "Dashboard" : route.path.substring(1)}
+                </Link>
+              </li>
+            ))}
+            {isAuthenticated() && (
+              <li>
+                <Link className="navbar-links" onClick={handleLogout}>
+                  Logout
+                </Link>
+              </li>
+            )}
+          </ul>
+        </div>
+
+        {/* Hamburger Icon */}
+        <div className="mobile-nav">
+          <FontAwesomeIcon
+            icon={faBars}
+            onClick={openNav}
+            className="hamb-icon"
+          />
+        </div>
+      </div>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          {routes.map((route, index) => (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <ProtectedRoute
+                  element={route.component}
+                  isAuth={route.isAuth}
+                />
+              }
+            />
+          ))}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </>
+  );
 }
